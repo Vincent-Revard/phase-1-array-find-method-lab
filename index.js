1c955af3bc7a221209f6a1d7a8d09ef2d179b06d
Vincent Revard <Vincent.Revard@gmail.com>
2024-02-03 15:39:19 -0800

    Lab Complete

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+]
+
+function superbowlWin(record) {
+    const winningYear = record.find(game => game.result === "W")
+    return winningYear ? winningYear.year : undefined;
+}
+
+// const superbowlWin = record => {
+//     const winningYear = record.find(game => game.result === "W")
+//     return winningYear ? winningYear.year : undefined;
+}
+/*
+function superbowlWin(record) {
+    const winningYear = record.find(game => game.result === "W")
+    if (winningYear) {
+        return winningYear.year
+    }   else {
+        return undefined
+        }
+    }
+
+*/
+    
+//! does not work
+// const superbowlWin = record.find.superbowlWIn((superbowlWin) => {
+//     if (superbowlWin.result === "W") {
+//         return superbowlWin
+//     }
+// })
+
+//const superbowlWIn = (record) => {
+//     return record.find((superbowl) => superbowl.result === "W")
+// }
+// const winningSuperbowl = superbowlWIn((record) => {
+// if (winningSuperbowl) {
+//     return (record.year);
+// } else {
+//     return undefined
+// }
+// })
